e6279c06836934635b8c5df57b84f536dc6a8113
anandapadmanabhan <anandapadmanabhan@gmail.com>
2021-04-02 21:07:06 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const connectDB = require('./config/db')
+
+const app = express();
+
+connectDB();
+
+app.get('/',(req,res)=>{
+    res.send('api running')
+})
+
+const PORT = process.env.PORT || 5000 
+
+app.listen(PORT)
